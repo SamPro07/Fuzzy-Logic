33078e793ba38a16d0c3d26629e9071e02b80d7f
SamPro07 <94083013+SamPro07@users.noreply.github.com>
2023-11-17 20:22:40 +0000

    Update unit.m

--- unit.m
+++ unit.m
@@ -1,77 +1,12 @@
-% To remove the warning messages for using old syntax
-warning('off','fuzzy:general:warnDeprecation_Newfis') 
-warning('off','fuzzy:general:warnDeprecation_Addvar')
-warning('off','fuzzy:general:warnDeprecation_Addmf')
-warning('off','fuzzy:general:warnDeprecation_Evalfis')
+Structure of a Rule with 11 Inputs
+A rule in a FIS with 11 inputs and, say, 2 outputs might look something like this:
 
-% Clears the Command Window of clutter
-clc
+***ruleX = [i1 i2 i3 i4 i5 i6 i7 i8 i9 i10 i11 o1 o2 w op];
 
-% Declare a new FIS
-%a = newfis('Cooling and Heating');
-
-%{
-newfis is a constructor which can be overloaded
-
-You can change the following:
-    ANDmethod = {min, prod}
-    Ormethod = {max, probor}
-    Implication = {min, prod}
-    Aggregation = {max, sum, probor}
-    Defuzzification = {centroid, bisector, mom, lom, som}
-%}
-
-%                                                   Sys Type,  AND, OR,  Impl, Agg    Defuzzification
-a=newfis('Cooling and Heating', 'mamdani', 'min', 'max', 'min', 'max', 'centroid');
-
-% Input 1: Day of Year:
-%1 = Jan 1st, 2 = Jan 2nd, ..., 31 = Jan 31st, 32 = Feb 1st
-a=addvar(a,'input','Day of Year',[0 365]);
-
-% Input 1's membership functions
-a = addmf(a, 'input', 1, 'Winter(1)', 'gaussmf', [25 0]);
-a = addmf(a, 'input', 1, 'Spring', 'gaussmf', [25 91.25]);
-a = addmf(a, 'input', 1, 'Summer', 'gaussmf', [25 182.5]);
-a = addmf(a, 'input', 1, 'Autumn', 'gaussmf', [25 273.75]);
-a = addmf(a, 'input', 1, 'Winter(2)', 'gaussmf', [25 365]);
-
-% Input 2: Time of Day (mins)
-a = addvar(a, 'input', 'Time of Day (mins)', [0 1440]);
-
-% Input 2's membership functions
-a = addmf(a, 'input', 2, 'Twilight(1)', 'trapmf', [0 0 60 240]); 
-a = addmf(a, 'input', 2, 'Morning', 'trapmf', [120 300 660 780]); 
-a = addmf(a, 'input', 2, 'Afternoon', 'trapmf', [660 780 1020 1140]); 
-a = addmf(a, 'input', 2, 'Evening', 'trapmf', [1020 1140 1320 1440]);
-a = addmf(a, 'input', 2, 'Twilight(2)', 'trapmf', [1200 1380 1440 1440]); 
-
-% Input 3: Temp (C)
-a = addvar(a, 'input', 'Temp (C)', [-20 40]);
-
-% Input 3's membership functions
-a = addmf(a, 'input', 3, 'V. Cold', 'trapmf', [-20 -20 0 5]);
-a = addmf(a, 'input', 3, 'Cold', 'trimf', [0 5 10]);
-a = addmf(a, 'input', 3, 'Moderate', 'trimf', [5 10 15]);
-a = addmf(a, 'input', 3, 'Warm', 'trimf', [10 15 20]);
-a = addmf(a, 'input', 3, 'V. Warm', 'trapmf', [15 20 40 40]);
-
-% Output 1: Cooling (%)
-a = addvar(a, 'output', 'Cooling', [-10 100]);
-
-% Output 1's membership functions
-a = addmf(a, 'output', 1, 'Off', 'trapmf', [-10 -10 0 0]);
-a = addmf(a, 'output', 1, 'Low', 'trapmf', [0 0 35 50]);
-a = addmf(a, 'output', 1, 'Moderate', 'trimf', [30 50 70]);
-a = addmf(a, 'output', 1, 'High', 'trapmf', [50 70 100 100]);
-
-% Output 2: Heating (%)
-a = addvar(a, 'output', 'Heating', [-10 100]);
-
-% Output 2's membership functions
-a = addmf(a, 'output', 2, 'Off', 'trapmf', [-10 -10 0 0]);
-a = addmf(a, 'output', 2, 'Low', 'trapmf', [0 0 35 50]);
-a = addmf(a, 'output', 2, 'Moderate', 'trimf', [30 50 70]);
-a = addmf(a, 'output', 2, 'High', 'trapmf', [50 70 100 100]);
+i1 to i11 are indices representing the states (membership functions) of the 11 inputs.
+o1, o2 are the indices for the output states.
+w is the weight of the rule.
+op is the operator used in the rule (usually AND or OR in Mamdani systems).
 
 % The rulebase 
 rule1 = [0 0 1 1 4 1 1];
